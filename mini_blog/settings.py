dc5ce1a79b1b772219bf4f65c6765b56f3ce7cf9
SK-Subroto <subroto35-1734@diu.edu.bd>
2022-02-16 01:11:22 +0600

    auth_middleware added

--- mini_blog/settings.py
+++ mini_blog/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'users.middlewares.auth_middleware',
+    'users.middlewares.auth_middleware',
 ]
 
 ROOT_URLCONF = 'mini_blog.urls'
